71693d5dfb0525e4c5381574d3f6205cb05f0c0c
Zoltan Kovacs <sifti85@gmail.com>
2023-01-26 10:59:17 +0100

    Project done responsiveness and vh fixed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+
 const buttons = document.querySelectorAll('.ratingButton');
 const submitButton = document.querySelector('#submitButton')
 const resultspan = document.querySelector('#result')
 
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
+    buttons[i].addEventListener('click', function (e) {
+        e.preventDefault();
         // clear every other button color
         ClearColor();
         // change clicked button's background color to orange and text color to white
@@ -14,11 +16,14 @@ for (let i = 0; i < buttons.length; i++) {
 // clear every other button color
 function ClearColor() {
     for (let j = 0; j < buttons.length; j++) {
-        buttons[j].classList.remove('active');
+        if (buttons[j].classList.contains('active')) {
+            buttons[j].classList.remove('active');
+        }
     }
 }
 //submitButton click
-submitButton.addEventListener("click", function () {
+submitButton.addEventListener("click", function (e) {
+    e.preventDefault();
     // check if any button has class active
     var active = document.querySelector(".ratingButton.active");
     if (active) {
